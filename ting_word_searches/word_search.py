00bc94e259227b2fdf99cfaaa67a59f69bf25a01
Felipe Matheus <fmatheusog@gmail.coM>
2022-09-29 01:35:39 -0300

    feat: create exists_word and search_by_word functions

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,45 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+
+    for index in range(len(instance.queue)):
+        has_word_in = []
+        file_data = instance.search(index)
+
+        for index, line in enumerate(file_data["linhas_do_arquivo"]):
+            if word.lower() in line.lower():
+                has_word_in.append({
+                    "linha": index + 1
+                })
+
+        if len(has_word_in) > 0:
+            result.append({
+                "palavra": word,
+                "arquivo": file_data["nome_do_arquivo"],
+                "ocorrencias": has_word_in
+            })
+
+    return result
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+
+    for index in range(len(instance.queue)):
+        has_word_in = []
+        file_data = instance.search(index)
+
+        for index, line in enumerate(file_data["linhas_do_arquivo"]):
+            if word.lower() in line.lower():
+                has_word_in.append({
+                    "linha": index + 1,
+                    "conteudo": line
+                })
+
+        if len(has_word_in) > 0:
+            result.append({
+                "palavra": word,
+                "arquivo": file_data["nome_do_arquivo"],
+                "ocorrencias": has_word_in
+            })
+
+    return result
