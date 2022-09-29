bc60c5187f7ca5218af5c54477e28cabda8bbfba
Felipe Matheus <fmatheusog@gmail.coM>
2022-09-29 00:14:18 -0300

    feat: create process function

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,26 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
+def check_file_exists(path_file, instance):
+    for item in range(len(instance.queue)):
+        return instance.search(item)["nome_do_arquivo"] == path_file
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    if not check_file_exists(path_file, instance):
+        file_lines = txt_importer(path_file)
+        qt_lines = len(file_lines)
+
+        file_data = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": qt_lines,
+            "linhas_do_arquivo": file_lines
+        }
+
+        instance.enqueue(file_data)
+        sys.stdout.write(str(file_data))
+
 
 
 def remove(instance):
